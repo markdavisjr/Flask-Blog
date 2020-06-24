9f6d98c01f40254e67491c3e7e5e7d4a712ea1b0
Mark Davis <markalandavisii@gmail.com>
2020-06-24 16:23:02 -0400

    second commit

--- flask_blog.py
+++ flask_blog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '79615371aafb9487a031687d98deed6f'
 
 posts = [
     {
@@ -24,6 +27,17 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)    
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)    
+
 
 #runs in debug without exporting environment variables
 '''if __name__ == '__main__':
